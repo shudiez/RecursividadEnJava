9b5198d2aded4d50e4f57916142a5fb7fef1bbe0
Yesica  V. Diez <69434927+shudiez@users.noreply.github.com>
2021-08-03 09:52:08 -0300

    Add files via upload

---
+++ Recursividad/src/clases/Clase_Principal.java
@@ -0,0 +1,8 @@
+package clases;
+
+public class Clase_Principal {
+    public static void main(String[] args) {
+        Recursividad recursividad = new Recursividad();
+        recursividad.Imprimir(1);
+    }
+}
---
+++ Recursividad/src/clases/Recursividad.java
@@ -0,0 +1,10 @@
+package clases;
+
+public class Recursividad {
+    public void Imprimir(int x){
+        if(x <= 5){
+            System.out.print(x + " ");
+            Imprimir(x + 1);
+        }
+    }
+}
